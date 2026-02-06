64bdd51252a0311c104fb63a47bb348e352d07e9
Meheraj H. <124564659+meheraj786@users.noreply.github.com>
2026-02-06 16:26:52 +0600

    minor

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -6,7 +6,7 @@ import { Send, CheckCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { Checkbox } from '../ui/checkbox';
+import { Checkbox } from '@/components/ui/checkbox';
 
 
 const contactReasons = [
